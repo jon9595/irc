21adc1a6f507ee831268bab18350e6faa33b0d1d
Jon Lantsberger <jtl378@mail.missouri.edu>
2017-04-05 16:32:04 -0500

    Maybe?

--- src/irc_client.c
+++ src/irc_client.c
@@ -2,3 +2,10 @@
 #include <stdlib.h>
 
 #include "irc_client.h"
+
+int main(int argc, char** argv) {
+
+    printf("Hello IRC Client: %d %s\n", argc, argv[0]);
+
+    return 0;
+}
--- src/irc_server.c
+++ src/irc_server.c
@@ -2,3 +2,10 @@
 #include <stdlib.h>
 
 #include "irc_server.h"
+
+int main(int argc, char** argv) {
+
+    printf("Hello IRC Server: %d %s\n", argc, argv[0]);
+
+    return 0;
+}
