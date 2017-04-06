89f5b2f9268880a2abfb7489ad888d16a2f8e1b4
Chris Jansson <cgj6hb@mail.missouri.edu>
2017-04-06 14:32:22 -0500

    Added functionality to store user login with timestamp

--- src/irc_client.c
+++ src/irc_client.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 #include "irc_client.h"
+
+int user_account_history_add_user(char*);
 
 int main(int argc, char** argv) {
     char username[11];
@@ -15,6 +18,26 @@ int main(int argc, char** argv) {
     printf("Username: ");
     scanf("%10s", username);
     printf("Welcome, %s!\n", username);
+
+    user_account_history_add_user(username);
+
+    return 0;
+}
+
+int user_account_history_add_user(char username[11]) {
+    // Get current datetime to store with username
+    time_t t = time(NULL);
+    struct tm *tm = localtime(&t);
+    char s[64];
+    strftime(s, sizeof(s), "%c", tm);
+
+    // Open file and append user
+    FILE* fp = fopen("../logs/user_account_history.txt", "a");
+    if (!fp) {
+        return 1;
+    } else {
+        fprintf(fp, "%s\n%s\n\n", username, s);
+    }
 
     return 0;
 }
